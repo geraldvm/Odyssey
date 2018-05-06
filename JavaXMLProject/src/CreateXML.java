bf8bf92f161a37438a0fd5e9074539a8c259c895
Gerald Valverde <gerald-_16@hotmail.com>
2018-05-05 21:44:44 -0600

    JavaXML

---
+++ JavaXMLProject/src/CreateXML.java
@@ -0,0 +1,54 @@
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+
+public class CreateXML {
+
+    public static void main(String[] args) throws Exception{
+
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+
+        Document document = documentBuilder.newDocument();
+
+        Element element = document.createElement("Developer");
+        document.appendChild(element);
+
+        Attr attr = document.createAttribute("ID");
+        attr.setValue("1");
+        element.setAttributeNode(attr);
+
+        Element name = document.createElement("Name");
+        name.appendChild(document.createTextNode("Gerald"));
+        element.appendChild(name);
+
+
+        Element surname = document.createElement("Surname");
+        surname.appendChild(document.createTextNode("Valverde"));
+        element.appendChild(surname);
+
+        Element age = document.createElement("Age");
+        age.appendChild(document.createTextNode("20"));
+        element.appendChild(age);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(document);
+
+        StreamResult streamResult = new StreamResult(new File("data.xml"));
+
+        transformer.transform(source,streamResult);
+
+
+
+
+    }
+}
---
+++ JavaXMLProject/src/ReadXML.java
@@ -0,0 +1,34 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
+public class ReadXML {
+
+    public static void main(String[] args) throws Exception{
+
+        File xmlFile = new File("data.xml");
+
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(xmlFile);
+
+        NodeList list = document.getElementsByTagName("Developer");
+
+        for(int i =0;i<list.getLength();i++){
+            Node node = list.item(i);
+
+            if(node.getNodeType()==Node.ELEMENT_NODE){
+                Element element=(Element) node;
+                System.out.println("ID: "+element.getAttribute("ID"));
+                System.out.println("Name: "+element.getElementsByTagName("Name").item(0).getTextContent());
+                System.out.println("Surname: "+element.getElementsByTagName("Surname").item(0).getTextContent());
+                System.out.println("Age: "+element.getElementsByTagName("Age").item(0).getTextContent());
+            }
+        }
+    }
+}
